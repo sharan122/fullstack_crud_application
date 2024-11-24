b08c2cb7ad772f7236e1755a4ec6434d0a2e886b
sharan122 <sharanraj037840@gmail.com>
2024-11-24 16:45:17 +0530

    admin backend completed finally

--- backend/urls.py
+++ backend/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('register/', register_user),
     path('users-list/',user_list,name = 'user-list'),
     path('add-user/', add_user, name='add-user'),
-    path('delete_user/<int:user_id>/', delete_user, name='delete_user'),
+    path('delete-user/<int:user_id>/', delete_user, name='delete_user'),
     path('edit-profile/',edit_profile, name='edit_profile'),
 ]
